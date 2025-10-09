daae66d347cd267f372b833447fb46bcc160d7ba
DRLK <davidmunvalle@gmail.com>
2025-10-09 19:20:03 +0200

    Mem + Vpp

--- rMetal/src/problem/problem_trait.rs
+++ rMetal/src/problem/problem_trait.rs
@@ -2,9 +2,8 @@ use crate::solutions::solution_trait::Solution;
 
 pub trait ProblemTrait<T: Clone> {
     /// Tipo de fitness que produce este problema
-    type Fitness: PartialOrd + Clone + std::fmt::Display + std::fmt::Debug;
     
-    fn evaluate<S: Solution<T, Fitness = Self::Fitness>>(&self, solution: &mut S);
+    fn evaluate<S: Solution<T>>(&self, solution: &mut S);
 
     
 }
