f713e034e030ebfe311dc1cbdeb0788685ec94c3
DRLK <davidmunvalle@gmail.com>
2026-07-25 19:14:03 +0200

    Estructure of Testing Folder

---
+++ roma/tests/observer.rs
@@ -0,0 +1,6 @@
+//! Integration tests for observer output and solution presentation.
+
+#[path = "observer/html_problem_presentation.rs"]
+mod html_problem_presentation;
+#[path = "observer/solution_presentation.rs"]
+mod solution_presentation;
